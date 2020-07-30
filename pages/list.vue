668a73ea19d53c0e904a80cb73f3c2c2d3699f73
谷津勝也 <fusatsukatsujin@gmail.com>
2020-07-30 22:42:29 +0900

    initial commit

---
+++ pages/list.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>aaaa</p>
+        <b-button>{{ hoge }}</b-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class List extends Vue {
+    hoge:string = "aaaaaaaaaaaaa"
+
+    get hogea() {
+        return "a"
+    }
+}
+</script>
